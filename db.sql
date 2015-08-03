697a0836d5472bda19a7580b9f85082517d8b5de
Ben <zpkx.wang@gmail.com>
2015-08-04 00:12:30 +0200

    sql script to allow remote client connection and populate db

--- db.sql
+++ db.sql
@@ -6,3 +6,14 @@ FLUSH PRIVILEGES;
 CREATE USER 'remote_root'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON *.* TO 'remote_root'@'%' WITH GRANT OPTION;
 
+#create new database
+create database if not exists lu_xuemei;
+use lu_xuemei;
+DROP TABLE IF EXISTS sensor;
+
+create TABLE sensor
+	(Id INT PRIMARY KEY AUTO_INCREMENT,
+	sensor_id INT UNSIGNED, 
+	sensor_value DECIMAL(4,2), 
+	timestamp TIMESTAMP);
+
